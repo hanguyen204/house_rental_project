16adb281a0b9790fcb73082e724c3a4aafc94416
hanguyen2004 <nongdangdinhmanh03@gmail.com>
2023-10-25 16:38:59 +0700

    admin see list user

--- src/main/java/com/example/rental_house_project/Service/IUserService.java
+++ src/main/java/com/example/rental_house_project/Service/IUserService.java
@@ -3,9 +3,18 @@ package com.example.rental_house_project.Service;
 
 import com.example.rental_house_project.Model.User;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface IUserService {
-    public void insertUser(User user) throws ClassNotFoundException;
+     void insertUser(User user) throws ClassNotFoundException;
 
 
-    public boolean checkEmail(String username);
+     boolean checkEmail(String username);
+
+    void updateStatusForUser(int id) throws SQLException, ClassNotFoundException;
+
+    void updateLockStatusForUser(int id) throws SQLException, ClassNotFoundException;
+
+    List<User> showAccUser() throws SQLException, ClassNotFoundException;
 }
